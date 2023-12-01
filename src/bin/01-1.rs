6e6ef0a8604ab96436958f3537e7cbc724f805bc
sarifb <saye.arif@gmail.com>
2023-12-01 12:18:29 +0200

    Completed day 1 part 1

--- src/bin/01-1.rs
+++ src/bin/01-1.rs
@@ -4,6 +4,13 @@ use std::fs;
 
 fn main() -> Result<()> {
   let data = fs::read_to_string("data/01.txt")?;
+  let sum: u32 = data
+    .lines()
+    .map(|line| line.chars().filter(|x| x.is_numeric()).collect_vec())
+    .map(|vec| [vec[0] as u8, vec[vec.len() - 1] as u8])
+    .flat_map(|slice| String::from_utf8_lossy(&slice).parse::<u32>())
+    .sum();
+  println!("{sum:?}");
 
   println!("Hello, from 01-1!");
   Ok(())
